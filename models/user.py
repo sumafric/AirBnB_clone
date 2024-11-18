8369b6038c521d83f33b6d67e4468603af9ea7af
BettyNyambura <btynyambura20@gmail.com>
2024-11-18 11:40:02 -0800

    mm

--- models/user.py
+++ models/user.py
@@ -12,3 +12,14 @@ class User(BaseModel):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+        # If attributes passed viakwags, init them
+         if kwargs:
+            if 'email' in kwargs:
+                self.email = kwargs['email']
+            if 'password' in kwargs:
+                self.password = kwargs['password']
+            if 'first_name' in kwargs:
+                self.first_name = kwargs['first_name']
+            if 'last_name' in kwargs:
+                self.last_name = kwargs['last_name']
